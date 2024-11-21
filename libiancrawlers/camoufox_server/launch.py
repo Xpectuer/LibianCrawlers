55deec1439361b60a207b88bcebc58c7cf19c9de
tcsnzh <zh.jobs@foxmail.com>
2024-11-21 17:11:27 +0800

    fix: use cli fire

--- libiancrawlers/camoufox_server/launch.py
+++ libiancrawlers/camoufox_server/launch.py
@@ -1,27 +1,35 @@
 # -*- coding: UTF-8 -*-
 from typing import Optional
+from loguru import logger
 
 from camoufox.server import launch_server
 
 
-def cli(*,
+def launch_server_from_cli(
+        *,
         headless=False,
         geoip=True,
         proxy_server: Optional[str] = None,
         proxy_username: Optional[str] = None,
         proxy_password: Optional[str] = None,
-        ):
+):
+    logger.debug('cli params : {}', locals())
     proxy = None if proxy_server is None and proxy_username is None and proxy_password is None else dict(
-        server=proxy_server,
-        username=proxy_username,
-        password=proxy_password,
+        **({} if proxy_server is None else dict(server=proxy_server)),
+        **({} if proxy_username is None else dict(username=proxy_username)),
+        **({} if proxy_password is None else dict(password=proxy_password)),
     )
-
+    logger.debug('proxy is {}', proxy)
     launch_server(
         headless=headless,
         geoip=geoip,
         proxy=proxy,
     )
+
+
+def cli():
+    from fire import Fire
+    Fire(launch_server_from_cli)
 
 
 if __name__ == '__main__':
